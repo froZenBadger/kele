d6be4ca4e6e5014c452cd6a68962b2f699fc95ee
Chris Palmer <cpalmer608@gmail.com>
2017-07-11 18:45:57 -0500

    initialized w bloc user to retrieve JWT

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,10 +1,15 @@
-require 'httparty'
+require 'HTTParty'
 
 class Kele
   include HTTParty
 
   def initialize(email, password)
-    @base_url = "https://www.bloc.io/api/v1/sessions"
-    # @token = JWToken
+    @base_url = "https://www.bloc.io/api/v1"
+    response = self.class.post(@base_url+"/sessions", body: {email: email, password: password})
+    @auth_token = response["auth_token"]
+    raise "Invalid email password combo" if @auth_token == nil
   end
+
+
+
 end
