87845f253e5897cadf610c96df9e79e194f9ee30
Chris Palmer <cpalmer608@gmail.com>
2017-07-12 15:44:19 -0500

    get_mentor_availability functioning

--- lib/kele.rb
+++ lib/kele.rb
@@ -7,13 +7,20 @@ class Kele
     @base_url = "https://www.bloc.io/api/v1"
     response = self.class.post(@base_url+"/sessions", body: {email: email, password: password})
     @auth_token = response["auth_token"]
-    raise "Invalid email password combo" if @auth_token == nil
+    raise "Invalid email password combo" if response.code != 200
   end
 
   def get_me
     url = @base_url+'/users/me'
     response = self.class.get(url, headers: { "authorization" => @auth_token })
     user_data = JSON.parse(response.body)
+    @mentor_id = user_data["current_enrollment"]["mentor_id"]
+    user_data
   end
 
+  def get_mentor_availability
+    url = @base_url+"/mentors/#{@mentor_id}/student_availability"
+    response = self.class.get(url, headers: { "authorization" => @auth_token }, body: {id: @mentor_id})
+    user_data = JSON.parse(response.body)
+  end
 end
