8c7ffa37814c3258b032dea4fbfa7035c907d72c
Chris Palmer <cpalmer608@gmail.com>
2017-07-14 15:49:06 -0500

    checkpoint submission

--- lib/kele.rb
+++ lib/kele.rb
@@ -19,6 +19,7 @@ class Kele
     @user_id = user_data["id"]
     @mentor_id = user_data["current_enrollment"]["mentor_id"]
     @roadmap_id = user_data["current_enrollment"]["roadmap_id"]
+    @enrollment_id = user_data["current_enrollment"]["id"]
   end
 
   def get_me
@@ -34,4 +35,16 @@ class Kele
     mentor_availability = JSON.parse(response.body)
   end
 
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    url = @base_url+"/checkpoint_submissions"
+    response = self.class.post(url,
+      headers: { "authorization" => @auth_token },
+      body: {
+        "assignment_branch" => assignment_branch,
+        "assignment_commit_link" => assignment_commit_link,
+        "checkpoint_id" => checkpoint_id,
+        "comment" => comment,
+        "enrollment_id" => @enrollment_id })
+    mentor_availability = JSON.parse(response.body)
+  end
 end
